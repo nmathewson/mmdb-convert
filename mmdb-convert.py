2da76cc2faf073681bfb3f6cc7564d2281d6b17c
Nick Mathewson <nickm@torproject.org>
2014-02-04 10:02:29 -0500

    Fix (non-)license to refer to the correct program

--- mmdb-convert.py
+++ mmdb-convert.py
@@ -3,9 +3,9 @@
 #   This software has been dedicated to the public domain under the CC0
 #   public domain dedication.
 #
-#   To the extent possible under law, the person who associated CC0 with
-#   libottery has waived all copyright and related or neighboring rights
-#   to libottery.
+#   To the extent possible under law, the person who associated CC0
+#   with mmdb-convert.py has waived all copyright and related or
+#   neighboring rights to mmdb-convert.py.
 #
 #   You should have received a copy of the CC0 legalcode along with this
 #   work in doc/cc0.txt.  If not, see
